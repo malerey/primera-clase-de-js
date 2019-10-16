4949e6c6eace32a410aa6d53f314cc7d0f5298f4
Malena Rey <malerey@male.5d640511d691.sn.mynetname.net>
2019-10-16 19:01:14 -0300

    first commit

---
+++ primerajavascript.js
@@ -0,0 +1,45 @@
+//console.log --> la manera en que mostramos informacion
+
+//declaramos una variable
+let nombre
+
+console.log(nombre)
+// le asignamos valor a una variable 
+nombre = "Malena"
+console.log(nombre)
+
+// podemos declarar y asignar valor a la vez
+let alumnaFavorita = "Ivana"
+console.log(alumnaFavorita)
+
+// podemos cambiar el valor de una variable 
+alumnaFavorita = "Lili"
+console.log(alumnaFavorita)
+
+alumnaFavorita = "Todas las remotas"
+console.log(alumnaFavorita)
+
+alumnaFavorita = "Sol"
+
+// los strings son cadenas de texto que Javascript no interpreta 
+// los numeros se escriben sin comillas 
+
+let numero = 2
+let otroNumero = 5
+
+// puedo sumar numeros, tanto los guardados en variables como numeros sin guardar
+console.log(5 + 7)
+
+console.log(numero + otroNumero)
+
+console.log(numero - otroNumero)
+
+console.log(numero * otroNumero)
+
+console.log(numero / otroNumero)
+
+// Operador modulo o resto, que me da el resto de una division
+console.log(numero % otroNumero) 
+
+
+console.log(numero + 10)
